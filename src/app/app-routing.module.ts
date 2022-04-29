d846f083efae39ac491eaa5e5e28a07f6a8b8511
JuergenHildbrand <jhildbrand@pm.me>
2022-04-29 15:06:18 +0200

    topCont responsive and add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { TopContainerComponent } from './top-container/top-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TopContainerComponent },
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopContainerComponent } from './top-container/top-container.component';
+import { HeaderComponent } from './header/header.component';
+import { ImprintComponent } from './imprint/imprint.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopContainerComponent,
+    HeaderComponent,
+    ImprintComponent,
+
   ],
   imports: [
     BrowserModule,
