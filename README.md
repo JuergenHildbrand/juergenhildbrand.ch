dbb379bef60f5bc8733c68050cd81b45989db7d3
JuergenHildbrand <jhildbrand@pm.me>
2022-05-04 15:00:06 +0200

    style optimize responsive an bug fix scrollbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Juergenhildbrand.Ch
+# Juergenhildbrand
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'juergenhildbrand.ch'`, () => {
+  it(`should have as title 'juergenhildbrand'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('juergenhildbrand.ch');
+    expect(app.title).toEqual('juergenhildbrand');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('juergenhildbrand.ch app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('juergenhildbrand app is running!');
   });
 });
