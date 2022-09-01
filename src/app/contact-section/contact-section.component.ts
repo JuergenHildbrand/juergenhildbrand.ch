17f2263477381ee7d6690a418ec47b881982cc8d
JuergenHildbrand <jhildbrand@pm.me>
2022-09-01 09:57:52 +0200

    about me text and animations changed

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -13,54 +13,27 @@ export class ContactSectionComponent {
   constructor(private http: HttpClient, public navigation: NavigationService) { }
 
   ///// Animations /////
-  titleContact = false;
-  description = false;
-  nameContact = false;
-  emailContact = false;
-  messageContact = false;
+  reachedContact = false;
   sendBtn = false;
   disabled = true;
+
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
 
-    const bodyRect = document.body.getBoundingClientRect();
-
     const screenTop = window.pageYOffset;
     const screenBottom = window.innerHeight + screenTop;
     const vw = window.innerWidth;
-    const vwCalc = vw / 12;
+    const vwCalc = vw / 8;
 
-    const a = document.getElementById('titleContact');
-    const title = a.getBoundingClientRect();
-    const titleBottomPosition = title.bottom - bodyRect.top;
+    const about = document.getElementById('contactSection');
+    const aboutHeight = about.clientHeight;
+    const aboutTopPosition = about.offsetTop;
+    const aboutBottomPosition = aboutTopPosition + aboutHeight;
 
-    const b = document.getElementById('description');
-    const description = b.getBoundingClientRect();
-    const descriptionBottomPosition = description.bottom - bodyRect.top;
-
-    const c = document.getElementById('name');
-    const nameContact = c.getBoundingClientRect();
-    const nameBottomPosition = nameContact.bottom - bodyRect.top;
-
-    const d = document.getElementById('email');
-    const email = d.getBoundingClientRect();
-    const emailBottomPosition = email.bottom - bodyRect.top;
-
-    const e = document.getElementById('message');
-    const message = e.getBoundingClientRect();
-    const messageBottomPosition = message.bottom - bodyRect.top;
-
-    const f = document.getElementById('sendBtn');
-    const sendBtn = f.getBoundingClientRect();
-    const sendBtnBottomPosition = sendBtn.bottom - bodyRect.top;
-
-    this.titleContact = screenBottom > titleBottomPosition - vwCalc;
-    this.description = screenBottom > descriptionBottomPosition - vwCalc;
-    this.nameContact = screenBottom > nameBottomPosition - vwCalc;
-    this.emailContact = screenBottom > emailBottomPosition - vwCalc;
-    this.messageContact = screenBottom > messageBottomPosition - vwCalc;
-    this.sendBtn = screenBottom > sendBtnBottomPosition - vwCalc;
+    this.reachedContact = screenBottom > aboutTopPosition + vwCalc && screenTop < aboutBottomPosition - vwCalc;
+    console.log(this.reachedContact);
+    
   }
 
   /**
